a8e13893072fdca638bc7b7056138c0477346d5e
Hervé Romary <h.romary@gmail.com>
2020-10-10 18:07:35 +0200

    git tag step-2

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,20 +1,4 @@
-// capitalizeFirstLetters.js
-const assert = require("assert");
-
-// WRITE THE ACTUAL FUNCTION HERE
-
-// Check that capitalizeFirstLetters is a function
-assert.strictEqual(typeof capitalizeFirst, "function");
-// Check that capitalizeFirstLetters accepts one argument
-assert.strictEqual(capitalizeFirst.length, 1);
-// Check that capitalizeFirstLetters transforms javaScript correctly
-assert.strictEqual(capitalizeFirst("hello world"), "Hello World");
-// Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirst("z"), "Z");
-// Check that it works for an empty string
-assert.strictEqual(capitalizeFirst(""), "");
-
-function capitalize(input) {
+function capitalizeFirstLetters(input) {
   const words = input.split(" ");
   const CapitalizedWords = [];
   words.forEach((element) => {
@@ -25,4 +9,4 @@ function capitalize(input) {
   return CapitalizedWords.join(" ");
 }
 
-capitalize("hello world");
+module.exports = capitalizeFirstLetters;
---
+++ test/capitalizeFirstLetters.test.js
@@ -0,0 +1,24 @@
+// capitalizeFirstLetters.js
+const assert = require("assert");
+const capitalizeFirst = require("../capitalizeFirstLetters");
+
+// give the test suite a label using describe
+describe("capitalizeFirstLetters", () => {
+  // give the test a label using it
+  it("is a function accepting one argument", () => {
+    assert.strictEqual(typeof capitalizeFirstLetters, "function");
+    assert.strictEqual(capitalizeFirstLetters.length, 1);
+  });
+
+  it("transforms hello world correctly", () => {
+    assert.strictEqual(capitalizeFirstLetters("hello world"), "Hello World");
+  });
+
+  it("works with a 1-character string", () => {
+    assert.strictEqual(capitalizeFirstLetters("z"), "Z");
+  });
+
+  it("works with an empty string", () => {
+    assert.strictEqual(capitalizeFirstLetters(""), "");
+  });
+});
