e36e18b6365ad3851ec0a24477e77da923c6c0ef
Hervé Romary <h.romary@gmail.com>
2020-10-10 17:39:28 +0200

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,23 +1,28 @@
 // capitalizeFirstLetters.js
-const assert = require('assert');
+const assert = require("assert");
 
 // WRITE THE ACTUAL FUNCTION HERE
 
 // Check that capitalizeFirstLetters is a function
-assert.strictEqual(typeof capitalizeFirst, 'function');
+assert.strictEqual(typeof capitalizeFirst, "function");
 // Check that capitalizeFirstLetters accepts one argument
 assert.strictEqual(capitalizeFirst.length, 1);
 // Check that capitalizeFirstLetters transforms javaScript correctly
-assert.strictEqual(capitalizeFirst('hello world'), 'Hello World');
+assert.strictEqual(capitalizeFirst("hello world"), "Hello World");
 // Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirst('z'), 'Z');
+assert.strictEqual(capitalizeFirst("z"), "Z");
 // Check that it works for an empty string
-assert.strictEqual(capitalizeFirst(''), '');
+assert.strictEqual(capitalizeFirst(""), "");
 
-
-
-
-function capitalizeFirstLetters {
-
+function capitalize(input) {
+  const words = input.split(" ");
+  const CapitalizedWords = [];
+  words.forEach((element) => {
+    CapitalizedWords.push(
+      element[0].toUpperCase() + element.slice(1, element.length)
+    );
+  });
+  return CapitalizedWords.join(" ");
 }
 
+capitalize("hello world");
